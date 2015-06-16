451b9fda23020899f51010df8cf0f6a25741cad7
Shaneen31 <Shaneen31@users.noreply.github.com>
2015-06-16 20:17:25 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# [Front-End Compilator](https://github.com/Shaneen31/Front-End-Compilator)
+
+[![Build Status](https://travis-ci.org/Shaneen31/Front-End-Compilator.svg)](https://travis-ci.org/Shaneen31/Front-End-Compilator)
+
+Version: 0.3.0
+
+
+## About
+This compilator is made to automate repetive task webdeveloppement, especially front-end.
+This scipt is made using  [node.js](https://nodejs.org/), [gulp](http://gulpjs.com/) and [libsass](http://libsass.org/).
+
+## Features
+
+ * SASS files compilation to CSS3.
+ * CSS3 minification.
+ * Automatic reload of pages on different browsers and devices.
+
+## Install and usage
+
+ * Copy gulpfile.js and package.json at root of your project.
+ * Open gulpfile.js and adjust settings (Path and general settings).
+ * In a termimal type npm install.
+ * run  the command gulp in a terminal to start automatic compilations of files.
+
+## Copyright and license
+
+Code and documentation copyright 2015 Shaneen31.
+Released under [the MIT license](https://github.com/Shaneen31/Front-End-Compilator/blob/master/LICENSE.md).
---
+++ gulpfile.js
@@ -0,0 +1,56 @@
+/*
+    Gulp Configuration File
+    Version: 1.0.0
+*/
+
+// Path settings.
+var sass_path = 'static/scss/',
+    css_path =  'static/css/',
+    js_path = 'static/js/',
+    html_path = 'templates/';
+
+// General settings.
+var production = false,
+    devServerAdress = '127.0.0.1:8000';
+
+// Browsers compatibility settings.
+ var browser_support = [
+    "ie >= 9",
+    "ie_mob >= 10",
+    "ie_mob >= 10",
+    "ff >= 30",
+    "chrome >= 34",
+    "safari >= 7",
+    "opera >= 23",
+    "ios >= 7",
+    "android >= 4.4",
+    "bb >= 10"
+];
+
+// Require.
+var gulp = require('gulp'),
+    gulpif = require('gulp-if'),
+    browserSync = require('browser-sync'),
+    plugins = require('gulp-load-plugins')();
+
+// Functions.
+// Compile SASS to CSS3 files.
+gulp.task('sass',function(){
+    gulp.src(sass_path.concat('**/*.scss'))
+        .pipe(plugins.plumber())
+        .pipe(plugins.sass())
+        .pipe(plugins.autoprefixer({browsers: browser_support, cascade: true}))
+        .pipe(gulpif(production, plugins.minifyCss()))
+        .pipe(gulp.dest(css_path));
+});
+
+// Watch source files, compile them and reload page in browser when a modification is made.
+gulp.task('default', ['sass'], function() {
+    browserSync.init({
+        proxy: devServerAdress
+    });
+    gulp.watch(css_path.concat('**/*.css'), browserSync.reload);
+    gulp.watch(html_path.concat('*.html'), browserSync.reload);
+    gulp.watch(js_path.concat('**/*.js'), browserSync.reload);
+    gulp.watch(sass_path.concat('**/*.scss'), ['sass']);
+});
